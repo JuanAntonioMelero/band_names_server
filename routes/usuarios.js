17d1e802a329b04a736846e3048eb12c5fc75a03
JuanAntonioMelero <57599434+JuanAntonioMelero@users.noreply.github.com>
2025-08-23 16:42:37 +0200

    Add files via upload

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,13 +4,32 @@
 */
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validarJWT');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validarCampos');
 
-const { getUsuarios } = require('../controllers/usuarios');
+const {
+    getUsuarios,
+    actualizarUsuario,
+    eliminarUsuario
+} = require('../controllers/usuarios');
 
 const router = Router();
 
+// Obtener usuarios
+router.get('/', validarJWT, getUsuarios);
 
+// Actualizar usuario
+router.put('/:id', [
+    validarJWT,
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('role', 'El role debe ser user o admin').isIn(['user', 'admin']),
+    validarCampos
+], actualizarUsuario);
 
-//router.get('/', validarJWT, getUsuarios);
-router.get('/', getUsuarios);
+// Eliminar usuario
+router.delete('/:id', [
+    validarJWT
+], eliminarUsuario);
+
 module.exports = router;
