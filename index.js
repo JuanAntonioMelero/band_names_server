61a972492c8e28e896040d90b6815254565a8796
JuanAntonioMelero <57599434+JuanAntonioMelero@users.noreply.github.com>
2025-08-23 15:57:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 
 require('dotenv').config();
 //DB config
-const {dbConecction} = require('./database/config');
+const { dbConecction } = require('./database/config');
 dbConecction();
 // App de Express
 const app = express();
@@ -15,14 +15,14 @@ app.use(express.json());
 // Node Server
 const server = require('http').createServer(app);
 module.exports.io = new Server(server, {
-          cors: {
-            //direcciones especificas
-            //origin: ["http://localhost:4200", "http://localhost:63621"],
-             //cualquier direccion
-             origin: '*',
-            methods: ["GET", "POST"]
-          }
-        });
+  cors: {
+    //direcciones especificas
+    //origin: ["http://localhost:53867", "http://localhost:63621"],
+    //cualquier direccion
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
 
 
 require('./sockets/socket');
@@ -31,19 +31,20 @@ require('./sockets/socket');
 
 
 // Path público
-const publicPath = path.resolve( __dirname, 'public' );
-app.use( express.static( publicPath ) );
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
 
 //Mis Rutas
 app.use('/api/login', require('./routes/auth'));
+app.use('/api/usuarios', require('./routes/usuarios'));
 
 
 
-server.listen( process.env.PORT, ( err ) => {
+server.listen(process.env.PORT, (err) => {
 
-    if ( err ) throw new Error(err);
+  if (err) throw new Error(err);
 
-    console.log('Servidor corriendo en puerto', process.env.PORT );
+  console.log('Servidor corriendo en puerto', process.env.PORT);
 
 });
 
